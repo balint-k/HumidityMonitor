73a8d6d31024a94a8b57368057a62951fd1ad66f
balint-k <balintkeri0515@gmail.com>
2026-03-21 23:28:34 +0100

    comment purge

--- C_Extension/GPIO_handler.h
+++ C_Extension/GPIO_handler.h
@@ -7,17 +7,8 @@
 #include <errno.h>
 #include <string.h>
 
-//AI generalta nyilvan a exmaple code-ból
 
-// Requests an input line and returns a gpiod_line_request object.
-// Returns NULL on failure.
-struct gpiod_line_request *request_input_line(const char *chip_path,
-					      unsigned int offset,
-					      const char *consumer);
-
-// Prints the value of a GPIO line (Active/Inactive).
-// Returns EXIT_SUCCESS on success, EXIT_FAILURE on error.
+struct gpiod_line_request *request_input_line(const char *chip_path, unsigned int offset, const char *consumer);
 int print_value(unsigned int offset, enum gpiod_line_value value);
-// Ez a fos amugy nem megy át valamiért .....
 
 #endif // GPIO_UTILS_H
