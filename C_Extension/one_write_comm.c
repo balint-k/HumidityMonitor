4c3276945aa7b9995854f2a795f3d6e503823a63
balint-k <balintkeri0515@gmail.com>
2026-03-21 15:34:54 +0100

    wait added

--- C_Extension/one_write_comm.c
+++ C_Extension/one_write_comm.c
@@ -1,6 +1,30 @@
 #include <stdio.h>
+#include <time.h>
+
+
 
 int main(){
-    printf("Hello Pi");
+    printf("Read Started");
+
+    int i;
+    uint64_t us = 10;
+    struct timespec start, now;
+
+    // 80 + 80 us -> 16 ~ 20 sample
+    // 40 bit
+    //    50 us low 30 us high (0) or 50 us low 70 us high (1)
+    //    Worst case: all high: 120 us -> 12 sample
+    // 40 * 12 = 480 sample
+    // 480 + 20 -> 500
+
+    for ( i = 0; i<520; i++ ){
+
+        clock_gettime(CLOCK_MONOTONIC, &start);
+        printf("Iteration %d\n", i);
+        do {
+            clock_gettime(CLOCK_MONOTONIC, &now);
+            uint64_t elapsed_ns = (now.tv_sec - start.tv_sec) * 1000000000L + (now.tv_nsec - start.tv_nsec);
+        } while (elapsed_ns < ns);
+    }
     return 0;
 }
