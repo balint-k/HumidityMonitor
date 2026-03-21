124d14e821a1ec626c85a0eeefac12c4ad8a893d
balint-k <balintkeri0515@gmail.com>
2026-03-21 23:41:18 +0100

    ;

--- C_Extension/GPIO_handler.h
+++ C_Extension/GPIO_handler.h
@@ -7,7 +7,7 @@
 #include <errno.h>
 #include <string.h>
 
-struct gpiod_line_request *request_output_line(const char *chip_path, unsigned int offset, enum gpiod_line_value value, const char *consumer)
+struct gpiod_line_request *request_output_line(const char *chip_path, unsigned int offset, enum gpiod_line_value value, const char *consumer);
 struct gpiod_line_request *request_input_line(const char *chip_path, unsigned int offset, const char *consumer);
 int print_value(unsigned int offset, enum gpiod_line_value value);
 
