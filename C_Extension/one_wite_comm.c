e3d2fc577a2afea059315d4034229397cf64ed9a
balint-k <balintkeri0515@gmail.com>
2026-03-21 23:02:21 +0100

    added read

--- C_Extension/one_wite_comm.c
+++ C_Extension/one_wite_comm.c
@@ -2,16 +2,35 @@
 #include <time.h>
 #include <stdint.h>
 
+#include <GPIO_handler.h>
+#include <errno.h>
+#include <gpiod.h>
+#include <stdlib.h>
+#include <string.h>
+
+
 
 int main(){
+
+    // ======== [Definition] =================================== // <- sose gondoltam hogy ezek valaha hasznosak lesznek lol
+
     printf("Read Started\n");
 
+    // Cuccok a GPIO kezeleshez
+	static const char *const chip_path = "/dev/gpiochip0";
+	static const unsigned int line_offset = 4;
+
+	struct gpiod_line_request *request;
+	enum gpiod_line_value value;
+	int ret;
+
+	
+    //Cuccok a mintavetelezeshet
     int i;
     uint64_t elapsed_ns;
     struct timespec start, now;
     uint64_t ns = 10 * 1000;
 
-    
     // 80 + 80 us -> 16 ~ 20 sample
     // 40 bit
     //    50 us low 30 us high (0) or 50 us low 70 us high (1)
@@ -19,12 +38,22 @@ int main(){
     // 40 * 12 = 480 sample
     // 480 + 20 -> 500
 
-    //trigger
 
+    // ======== [Trigger] =================================== //
+
+
+    // ======== [Read] =================================== //
+    request = request_input_line(chip_path, line_offset, "get-line-value");
+    if (!request) {
+		fprintf(stderr, "failed to request line: %s\n",
+			strerror(errno));
+		return EXIT_FAILURE;
+	}
     for ( i = 0; i<520; i++ ){
 
         clock_gettime(CLOCK_MONOTONIC, &start);
-        printf("Iteration %d\n", i);
+        value = gpiod_line_request_get_value(request, line_offset);
+	    ret = print_value(line_offset, value);
         // Read .....
         do {
             clock_gettime(CLOCK_MONOTONIC, &now);
@@ -32,5 +61,9 @@ int main(){
             printf("Wait...\n");
         } while (elapsed_ns < ns);
     }
+
+	/* not strictly required here, but if the app wasn't exiting... */
+	gpiod_line_request_release(request);
+
     return 0;
 }
