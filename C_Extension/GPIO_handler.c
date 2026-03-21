108fdcda77d26e9fb14c634741b519de132f8d84
balint-k <balintkeri0515@gmail.com>
2026-03-21 23:08:44 +0100

    main deleted

--- C_Extension/GPIO_handler.c
+++ C_Extension/GPIO_handler.c
@@ -73,29 +73,3 @@ static int print_value(unsigned int offset, enum gpiod_line_value value)
 
 	return EXIT_SUCCESS;
 }
-
-int main(void)
-{
-	/* Example configuration - customize to suit your situation. */
-	static const char *const chip_path = "/dev/gpiochip0";
-	static const unsigned int line_offset = 4;
-
-	struct gpiod_line_request *request;
-	enum gpiod_line_value value;
-	int ret;
-
-	request = request_input_line(chip_path, line_offset, "get-line-value");
-	if (!request) {
-		fprintf(stderr, "failed to request line: %s\n",
-			strerror(errno));
-		return EXIT_FAILURE;
-	}
-
-	value = gpiod_line_request_get_value(request, line_offset);
-	ret = print_value(line_offset, value);
-
-	/* not strictly required here, but if the app wasn't exiting... */
-	gpiod_line_request_release(request);
-
-	return ret;
-}
