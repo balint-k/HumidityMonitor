65200d589cc7796b6b3f1fd04dbacc6bc2578e4c
balint-k <balintkeri0515@gmail.com>
2026-03-21 22:35:18 +0100

    the example code copied

--- C_Extension/GPIO_handler.c
+++ C_Extension/GPIO_handler.c
@@ -1,52 +1,106 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+// SPDX-FileCopyrightText: 2023 Kent Gibson <warthog618@gmail.com>
+
+/* Minimal example of reading a single line. */
+
+#include <errno.h>
 #include <gpiod.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-#define	CONSUMER	"Consumer"
-
-int main(){
-    //gpiodetect 
-    //gpiochip0 [pinctrl-bcm2835] (54 lines) <- ez kell
-    //gpiochip1 [raspberrypi-exp-gpio] (8 lines)
-
-    int line_num = 4;
-    int val;
+/* Request a line as input. */
+static struct gpiod_line_request *request_input_line(const char *chip_path,
+						     unsigned int offset,
+						     const char *consumer)
+{
+	struct gpiod_request_config *req_cfg = NULL;
+	struct gpiod_line_request *request = NULL;
+	struct gpiod_line_settings *settings;
+	struct gpiod_line_config *line_cfg;
 	struct gpiod_chip *chip;
-	struct gpiod_line *line;
-	int i, ret;
+	int ret;
 
-	chip = gpiod_chip_open_by_name("gpiochip0");
-	if (!chip) {
-		perror("Open chip failed\n");
-		goto end;
-	}
+	chip = gpiod_chip_open(chip_path);
+	if (!chip)
+		return NULL;
 
-	line = gpiod_chip_get_line(chip, line_num);
-	if (!line) {
-		perror("Get line failed\n");
+	settings = gpiod_line_settings_new();
+	if (!settings)
 		goto close_chip;
+
+	gpiod_line_settings_set_direction(settings, GPIOD_LINE_DIRECTION_INPUT);
+
+	line_cfg = gpiod_line_config_new();
+	if (!line_cfg)
+		goto free_settings;
+
+	ret = gpiod_line_config_add_line_settings(line_cfg, &offset, 1,
+						  settings);
+	if (ret)
+		goto free_line_config;
+
+	if (consumer) {
+		req_cfg = gpiod_request_config_new();
+		if (!req_cfg)
+			goto free_line_config;
+
+		gpiod_request_config_set_consumer(req_cfg, consumer);
 	}
 
-	ret = gpiod_line_request_input(line, CONSUMER);
-	if (ret < 0) {
-		perror("Request line as input failed\n");
-		goto release_line;
+	request = gpiod_chip_request_lines(chip, req_cfg, line_cfg);
+
+	gpiod_request_config_free(req_cfg);
+
+free_line_config:
+	gpiod_line_config_free(line_cfg);
+
+free_settings:
+	gpiod_line_settings_free(settings);
+
+close_chip:
+	gpiod_chip_close(chip);
+
+	return request;
+}
+
+static int print_value(unsigned int offset, enum gpiod_line_value value)
+{
+	if (value == GPIOD_LINE_VALUE_ACTIVE)
+		printf("%d=Active\n", offset);
+	else if (value == GPIOD_LINE_VALUE_INACTIVE) {
+		printf("%d=Inactive\n", offset);
+	} else {
+		fprintf(stderr, "error reading value: %s\n",
+			strerror(errno));
+		return EXIT_FAILURE;
 	}
 
-	/* Read input 20 times */
-	for (i = 20; i > 0; i--) {
-		val = gpiod_line_get_value(line);
-		if (val < 0) {
-			perror("Read line input failed\n");
-			goto release_line;
-		}
-		printf("Intput %d on line #%u\n", val, line_num);
-		sleep(1);
+	return EXIT_SUCCESS;
+}
+
+int main(void)
+{
+	/* Example configuration - customize to suit your situation. */
+	static const char *const chip_path = "/dev/gpiochip0";
+	static const unsigned int line_offset = 4;
+
+	struct gpiod_line_request *request;
+	enum gpiod_line_value value;
+	int ret;
+
+	request = request_input_line(chip_path, line_offset, "get-line-value");
+	if (!request) {
+		fprintf(stderr, "failed to request line: %s\n",
+			strerror(errno));
+		return EXIT_FAILURE;
 	}
 
-    release_line:
-        gpiod_line_release(line);
-    close_chip:
-        gpiod_chip_close(chip);
-    end:
-        return 0;
+	value = gpiod_line_request_get_value(request, line_offset);
+	ret = print_value(line_offset, value);
+
+	/* not strictly required here, but if the app wasn't exiting... */
+	gpiod_line_request_release(request);
+
+	return ret;
 }
